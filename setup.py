4f4ed95671d970537e795716f6556cf56eda50cc
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-05-10 14:32:58 +0200

    No longer provide scenario files in this package.

--- setup.py
+++ setup.py
@@ -9,11 +9,9 @@ setup(
             '/*.yml',
         ],
     },
-
     entry_points={
         'console_scripts': [
             'scenario-player=scenario_player.__main__:main',
-            'scenario-player-nightlies=scenario_player.scenarios.__init__:run_builtin_scenarios',
         ],
     },
     requires=[
